2e292bfd73c9d60a61c36f5767cc455f36c067c5
h1w <bpqvgq@gmail.com>
2026-02-13 13:20:00 +0700

    Add glog and custom logger sink

---
+++ wotlk-utils/main.cpp
@@ -0,0 +1,55 @@
+﻿#include "glog_custom_formatter.hpp"
+
+void setupLogger(const char* program_name)
+{
+	// конфигруация логгера
+	GlogCustomFormatter::Config cfg;
+
+	// уроверь логирования
+	cfg.min_log_level = google::GLOG_INFO;
+	cfg.verbose_level = 2;  // VLOG(1), VLOG(2) будут работать
+
+	// консоль
+	cfg.console_enabled = true;
+	cfg.console_use_color = true;
+
+	// плейсхолдеры
+	//   %L  — уровень полное слово  (INFO / WARN / ERROR / FATAL)
+	//   %l  — уровень один символ   (I / W / E / F)
+	//   %T  — время YYYY-MM-DD HH:MM:SS.uuuuuu
+	//   %F  — имя исходного файла
+	//   %N  — номер строки
+	//   %P  — PID
+	//   %I  — Thread ID
+	//   %M  — тело сообщения
+	cfg.console_pattern = "[%L] %T [%F:%N] %M";
+
+	// файл
+	cfg.file_enabled = true;
+	cfg.log_directory = "./logs";
+	cfg.base_filename = "wotlk_utils";
+	// В файле формат в стиле glog: << Lmmdd hh:mm:ss.uuuuuu pid tid file:line] msg >>
+	cfg.file_pattern = "%l%T %P %I %F:%N] %M";
+
+	// ротация
+	cfg.max_file_size_mb = 50;   // ротация при 50 МБ
+	cfg.max_file_count = 5;   // хранить до 5 файлов
+	cfg.max_file_age_hours = 168;  // удалять файлы старше 7 дней
+	cfg.rotate_daily = true; // новый файл каждый день
+
+	// инициализация
+	GlogCustomFormatter& formatter = GlogCustomFormatter::Instance();
+	formatter.Initialize(cfg, program_name);
+}
+
+int main(int argc, char** argv)
+{
+	setupLogger(argv[0]);
+
+	// тестирование логирования
+	LOG(INFO) << "Приложение запущено";
+	LOG(WARNING) << "Это предупреждение";
+	LOG(ERROR) << "Это ошибка";
+
+	return 0;
+}
